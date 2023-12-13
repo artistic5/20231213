083aab8ece5e868604b36f0ba6e46e14ca4b3110
artistic5 <artisticsoftware5@gmail.com>
2023-12-13 11:20:27 +0000

    ...

--- 1213/1.php
+++ 1213/1.php
@@ -36,15 +36,14 @@ return [
 		/**
 		Race 3
 		*/
-		'Win Odds'  =>  '2, 1, 9, 12, 6, 11, 7, 3, 4, 8, 10, 5',
-		'Pla Odds'  =>  '2, 9, 1, 12, 11, 6, 7, 3, 4, 10, 8, 5',
+		'Win Odds'  =>  '12, 2, 1, 9, 6, 11, 7, 3, 4, 8, 10, 5',
+		'Pla Odds'  =>  '9, 12, 2, 1, 11, 6, 7, 3, 4, 8, 10, 5',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		'win odds candidate(k = 7)' => '11',
 		/** Black selection */
-		'history w' => '11, 6',
+		'history w' => '6, 11',
 		/** Based on pla odds */
-		'pla odds candidate(k = 10)' => '1',
 		'pla odds candidate(k = 7)' => '6',
 		'history p' => '11, 6',
 	],
@@ -74,7 +73,7 @@ return [
 		Race 6
 		*/
 		'Win Odds'  =>  '4, 1, 7, 10, 6, 3, 11, 12, 5, 9, 2, 8',
-		'Pla Odds'  =>  '4, 10, 1, 7, 6, 3, 5, 12, 11, 9, 2, 8',
+		'Pla Odds'  =>  '4, 10, 1, 7, 6, 3, 12, 5, 11, 9, 2, 8',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		/** Based on pla odds */
@@ -97,7 +96,7 @@ return [
 		Race 8
 		*/
 		'Win Odds'  =>  '7, 5, 8, 12, 9, 3, 4, 1, 2, 6, 10, 11',
-		'Pla Odds'  =>  '7, 12, 8, 3, 9, 5, 1, 4, 2, 11, 6, 10',
+		'Pla Odds'  =>  '7, 12, 8, 5, 9, 3, 1, 4, 2, 11, 6, 10',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		/** Based on pla odds */
@@ -106,12 +105,12 @@ return [
 		/**
 		Race 9
 		*/
-		'Win Odds'  =>  '6, 7, 11, 1, 10, 3, 4, 8, 5, 9, 2, 12',
+		'Win Odds'  =>  '6, 7, 11, 1, 3, 10, 4, 8, 5, 9, 2, 12',
 		'Pla Odds'  =>  '6, 7, 11, 10, 3, 4, 1, 8, 9, 5, 2, 12',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		'win odds candidate(k = 12)' => '6',
-		'win odds candidate(k = 8)' => '10',
+		'win odds candidate(k = 8)' => '3',
 		/** Black selection */
 		'history w' => '6, 10',
 		'SURE PLACE' => '6',
