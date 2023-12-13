f37a435abfc6495622acb8be7707772267873f10
artistic5 <artisticsoftware5@gmail.com>
2023-12-13 11:29:52 +0000

    ...

--- 1213/1.php
+++ 1213/1.php
@@ -36,23 +36,22 @@ return [
 		/**
 		Race 3
 		*/
-		'Win Odds'  =>  '12, 2, 1, 9, 6, 11, 7, 3, 4, 8, 10, 5',
-		'Pla Odds'  =>  '9, 12, 2, 1, 11, 6, 7, 3, 4, 8, 10, 5',
+		'Win Odds'  =>  '12, 2, 1, 9, 6, 7, 11, 3, 4, 8, 10, 5',
+		'Pla Odds'  =>  '12, 9, 2, 1, 6, 11, 7, 3, 4, 8, 10, 5',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
-		'win odds candidate(k = 7)' => '11',
 		/** Black selection */
 		'history w' => '6, 11',
 		/** Based on pla odds */
-		'pla odds candidate(k = 7)' => '6',
-		'history p' => '11, 6',
+		'pla odds candidate(k = 7)' => '11',
+		'history p' => '6, 11',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'Win Odds'  =>  '8, 6, 5, 1, 3, 2, 12, 9, 11, 7, 4, 10',
-		'Pla Odds'  =>  '6, 8, 5, 1, 3, 2, 12, 9, 11, 7, 4, 10',
+		'Pla Odds'  =>  '6, 5, 8, 1, 3, 2, 12, 9, 11, 7, 4, 10',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		/** Based on pla odds */
@@ -73,7 +72,7 @@ return [
 		Race 6
 		*/
 		'Win Odds'  =>  '4, 1, 7, 10, 6, 3, 11, 12, 5, 9, 2, 8',
-		'Pla Odds'  =>  '4, 10, 1, 7, 6, 3, 12, 5, 11, 9, 2, 8',
+		'Pla Odds'  =>  '4, 10, 1, 7, 6, 3, 5, 12, 11, 9, 2, 8',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		/** Based on pla odds */
@@ -82,11 +81,10 @@ return [
 		/**
 		Race 7
 		*/
-		'Win Odds'  =>  '8, 2, 9, 7, 4, 3, 11, 1, 6, 10, 12, 5',
+		'Win Odds'  =>  '8, 2, 9, 7, 4, 3, 11, 10, 1, 6, 12, 5',
 		'Pla Odds'  =>  '8, 2, 7, 9, 3, 4, 11, 10, 1, 6, 12, 5',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
-		'win odds candidate(k = 10)' => '9',
 		/** Black selection */
 		'history w' => '4',
 		/** Based on pla odds */
@@ -95,7 +93,7 @@ return [
 		/**
 		Race 8
 		*/
-		'Win Odds'  =>  '7, 5, 8, 12, 9, 3, 4, 1, 2, 6, 10, 11',
+		'Win Odds'  =>  '7, 5, 8, 12, 9, 4, 3, 1, 2, 6, 10, 11',
 		'Pla Odds'  =>  '7, 12, 8, 5, 9, 3, 1, 4, 2, 11, 6, 10',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
@@ -105,19 +103,19 @@ return [
 		/**
 		Race 9
 		*/
-		'Win Odds'  =>  '6, 7, 11, 1, 3, 10, 4, 8, 5, 9, 2, 12',
-		'Pla Odds'  =>  '6, 7, 11, 10, 3, 4, 1, 8, 9, 5, 2, 12',
+		'Win Odds'  =>  '6, 7, 11, 1, 10, 3, 4, 8, 5, 9, 2, 12',
+		'Pla Odds'  =>  '6, 7, 11, 3, 10, 4, 1, 8, 9, 5, 2, 12',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		'win odds candidate(k = 12)' => '6',
-		'win odds candidate(k = 8)' => '3',
+		'win odds candidate(k = 8)' => '10',
 		/** Black selection */
 		'history w' => '6, 10',
 		'SURE PLACE' => '6',
 		/** Based on pla odds */
 		'pla odds candidate(k = 12)' => '6',
-		'pla odds candidate(k = 9)' => '10',
-		'pla odds candidate(k = 8)' => '3',
+		'pla odds candidate(k = 9)' => '3',
+		'pla odds candidate(k = 8)' => '10',
 		'history p' => '6, 10, 4, 2',
 		'SURE PLACE' => '6',
 	],
