cc671d417aedb3033f599de57648955f03f39d26
artistic5 <artisticsoftware5@gmail.com>
2023-12-13 11:03:40 +0000

    ...

--- 1213/1.php
+++ 1213/1.php
@@ -18,8 +18,8 @@ return [
 		/**
 		Race 2
 		*/
-		'Win Odds'  =>  '6, 1, 10, 2, 9, 7, 3, 11, 8, 5, 4, 12',
-		'Pla Odds'  =>  '6, 10, 1, 2, 7, 9, 3, 11, 8, 5, 4, 12',
+		'Win Odds'  =>  '6, 10, 1, 2, 9, 7, 11, 3, 8, 5, 4, 12',
+		'Pla Odds'  =>  '6, 10, 1, 9, 7, 2, 8, 11, 3, 5, 4, 12',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		'win odds candidate(k = 12)' => '6',
@@ -36,7 +36,7 @@ return [
 		Race 3
 		*/
 		'Win Odds'  =>  '2, 1, 9, 12, 11, 6, 7, 3, 4, 8, 10, 5',
-		'Pla Odds'  =>  '2, 9, 1, 12, 11, 7, 6, 3, 4, 10, 8, 5',
+		'Pla Odds'  =>  '2, 9, 1, 12, 11, 6, 7, 3, 4, 10, 8, 5',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		'win odds candidate(k = 7)' => '6',
@@ -44,14 +44,15 @@ return [
 		'history w' => '11, 6',
 		/** Based on pla odds */
 		'pla odds candidate(k = 10)' => '1',
+		'pla odds candidate(k = 7)' => '6',
 		'history p' => '11, 6',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
-		'Win Odds'  =>  '8, 6, 5, 1, 3, 2, 9, 12, 11, 4, 7, 10',
-		'Pla Odds'  =>  '6, 5, 1, 3, 8, 2, 9, 12, 11, 7, 4, 10',
+		'Win Odds'  =>  '8, 6, 5, 1, 3, 2, 12, 9, 11, 7, 4, 10',
+		'Pla Odds'  =>  '6, 8, 5, 1, 3, 12, 2, 9, 11, 7, 4, 10',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		/** Based on pla odds */
@@ -60,8 +61,8 @@ return [
 		/**
 		Race 5
 		*/
-		'Win Odds'  =>  '6, 9, 4, 5, 1, 3, 10, 7, 8',
-		'Pla Odds'  =>  '6, 9, 4, 5, 10, 1, 3, 7, 8',
+		'Win Odds'  =>  '6, 9, 5, 4, 1, 3, 10, 7, 8',
+		'Pla Odds'  =>  '6, 9, 4, 5, 10, 3, 1, 7, 8',
 		'Total Runners '  =>  9,
 		/** Based on win odds */
 		/** Based on pla odds */
@@ -93,8 +94,8 @@ return [
 		/**
 		Race 8
 		*/
-		'Win Odds'  =>  '7, 5, 8, 12, 3, 4, 9, 1, 2, 6, 10, 11',
-		'Pla Odds'  =>  '7, 12, 8, 3, 9, 5, 1, 4, 2, 11, 6, 10',
+		'Win Odds'  =>  '7, 5, 8, 12, 4, 9, 3, 1, 2, 6, 10, 11',
+		'Pla Odds'  =>  '7, 12, 8, 3, 5, 9, 1, 4, 2, 11, 6, 10',
 		'Total Runners '  =>  12,
 		/** Based on win odds */
 		/** Based on pla odds */
