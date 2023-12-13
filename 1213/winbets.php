9923f7606c1206cc97b100c2fcd015fcec20d27a
artistic5 <artisticsoftware5@gmail.com>
2023-12-13 12:00:53 +0000

    ...

--- 1213/winbets.php
+++ 1213/winbets.php
@@ -76,24 +76,25 @@ return [
 		/**
 		Race 4
 		*/
+		'favorite set'  =>  'Set 2',
 		'Set 1 Win Bets'  =>  [
-			'8' => '30 HKD',
-			'6' => '30 HKD',
-			'3' => '20 HKD',
-			'12' => '10 HKD',
+			'8' => '20 HKD',
+			'6' => '20 HKD',
+			'3' => '10 HKD',
 			'9' => '10 HKD',
 			'4' => '10 HKD',
 		],
-		'Total Bets set 1'  =>  '110 HKD',
+		'Total Bets set 1'  =>  '70 HKD',
 		'Set 2 Win Bets'  =>  [
-			'5' => '10 HKD',
-			'1' => '10 HKD',
+			'5' => '20 HKD',
+			'1' => '20 HKD',
 			'2' => '10 HKD',
+			'12' => '10 HKD',
 			'11' => '10 HKD',
 			'7' => '10 HKD',
 			'10' => '10 HKD',
 		],
-		'Total Bets set 2'  =>  '60 HKD'
+		'Total Bets set 2'  =>  '90 HKD'
 	],
 	'5' => [
 		/**
@@ -124,20 +125,20 @@ return [
 			'1' => '20 HKD',
 			'10' => '20 HKD',
 			'6' => '20 HKD',
-			'3' => '20 HKD',
-			'11' => '10 HKD',
+			'3' => '10 HKD',
 			'12' => '10 HKD',
 			'9' => '10 HKD',
 		],
-		'Total Bets set 1'  =>  '110 HKD',
+		'Total Bets set 1'  =>  '90 HKD',
 		'Set 2 Win Bets'  =>  [
-			'4' => '30 HKD',
+			'4' => '40 HKD',
 			'7' => '20 HKD',
+			'11' => '10 HKD',
 			'5' => '10 HKD',
 			'2' => '10 HKD',
 			'8' => '10 HKD',
 		],
-		'Total Bets set 2'  =>  '80 HKD'
+		'Total Bets set 2'  =>  '100 HKD'
 	],
 	'7' => [
 		/**
@@ -145,23 +146,23 @@ return [
 		*/
 		'favorite set'  =>  'Set 2',
 		'Set 1 Win Bets'  =>  [
-			'2' => '10 HKD',
+			'2' => '20 HKD',
 			'4' => '10 HKD',
+			'1' => '10 HKD',
 		],
-		'Total Bets set 1'  =>  '20 HKD',
+		'Total Bets set 1'  =>  '40 HKD',
 		'Set 2 Win Bets'  =>  [
 			'8' => '80 HKD',
 			'7' => '30 HKD',
 			'9' => '30 HKD',
 			'3' => '20 HKD',
 			'11' => '20 HKD',
-			'10' => '20 HKD',
-			'1' => '20 HKD',
-			'6' => '20 HKD',
+			'10' => '10 HKD',
+			'6' => '10 HKD',
 			'12' => '10 HKD',
 			'5' => '10 HKD',
 		],
-		'Total Bets set 2'  =>  '260 HKD'
+		'Total Bets set 2'  =>  '220 HKD'
 	],
 	'8' => [
 		/**
