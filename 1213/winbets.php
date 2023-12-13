a9bc7965b9a6b164fe6e6b46967d079c91067146
artistic5 <artisticsoftware5@gmail.com>
2023-12-13 13:57:02 +0000

    ...

--- 1213/winbets.php
+++ 1213/winbets.php
@@ -146,13 +146,13 @@ return [
 		'favorite set'  =>  'Set 2',
 		'Set 1 Win Bets'  =>  [
 			'2' => '10 HKD',
+			'7' => '10 HKD',
 			'4' => '10 HKD',
 		],
-		'Total Bets set 1'  =>  '20 HKD',
+		'Total Bets set 1'  =>  '30 HKD',
 		'Set 2 Win Bets'  =>  [
 			'8' => '0 HKD',
 			'9' => '0 HKD',
-			'7' => '0 HKD',
 			'11' => '0 HKD',
 			'3' => '0 HKD',
 			'10' => '0 HKD',
@@ -168,46 +168,45 @@ return [
 		Race 8
 		*/
 		'Set 1 Win Bets'  =>  [
-			'7' => '30 HKD',
-			'5' => '30 HKD',
-			'12' => '20 HKD',
-			'3' => '20 HKD',
-			'9' => '20 HKD',
+			'7' => '20 HKD',
+			'4' => '20 HKD',
+			'9' => '10 HKD',
+			'12' => '10 HKD',
+			'3' => '10 HKD',
 			'6' => '10 HKD',
 		],
-		'Total Bets set 1'  =>  '130 HKD',
+		'Total Bets set 1'  =>  '80 HKD',
 		'Set 2 Win Bets'  =>  [
-			'8' => '10 HKD',
-			'4' => '10 HKD',
+			'5' => '20 HKD',
+			'8' => '20 HKD',
 			'1' => '10 HKD',
 			'2' => '10 HKD',
 			'10' => '10 HKD',
 			'11' => '10 HKD',
 		],
-		'Total Bets set 2'  =>  '60 HKD'
+		'Total Bets set 2'  =>  '80 HKD'
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
-		'favorite set'  =>  'Set 2',
 		'Set 1 Win Bets'  =>  [
 			'7' => '20 HKD',
 			'1' => '10 HKD',
+			'3' => '10 HKD',
 			'10' => '10 HKD',
 			'9' => '10 HKD',
 			'12' => '10 HKD',
 		],
-		'Total Bets set 1'  =>  '60 HKD',
+		'Total Bets set 1'  =>  '70 HKD',
 		'Set 2 Win Bets'  =>  [
-			'6' => '60 HKD',
-			'11' => '40 HKD',
-			'3' => '20 HKD',
-			'4' => '20 HKD',
+			'6' => '40 HKD',
+			'11' => '30 HKD',
+			'4' => '10 HKD',
 			'8' => '10 HKD',
 			'5' => '10 HKD',
 			'2' => '10 HKD',
 		],
-		'Total Bets set 2'  =>  '170 HKD'
+		'Total Bets set 2'  =>  '110 HKD'
 	],
 ];
